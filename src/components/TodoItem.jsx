82d594c39bb47512c9e2046afdf3f18af03f49d3
Nikolai A. <ilowen@ya.ru>
2024-02-08 16:56:22 +0800

    New: Implemented an animation for removing todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import styles from '../css/TodoItem.module.css';
 
 // react
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // icons
 import { HiMiniPencilSquare } from "react-icons/hi2";
@@ -9,6 +9,7 @@ import { FaTrash } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 
 function TodoItem({ index, title, id, bin, isCompleted, onRenameTodo, onRemoveTodo, onMarkTodo }) {
+	const todoRef = useRef(null);
 	const [visible, setVisible] = useState(false);
 
 	useEffect(() => {
@@ -17,18 +18,26 @@ function TodoItem({ index, title, id, bin, isCompleted, onRenameTodo, onRemoveTo
 		}, index * 100);
 	}, []);
 
+	const handleRemove = () => {
+		todoRef.current.classList.add(styles.removingItem);
+
+		setTimeout(() => {
+			onRemoveTodo(bin, id, styles);
+		}, 600);
+	}
+
 	return (
-		<div data-id={id} className={`${styles.todoItemWrapper} ${visible ? styles.visible : ''}`}>
+		<li data-id={id} ref={todoRef} className={`${styles.todoItemWrapper} ${visible ? styles.visible : ''}`}>
 			<div className={`${styles.todoItem} ${isCompleted ? styles.isCompleted : ''}`}>
 				<span className={styles.text}>{title}</span>
 			</div>
 
 			<div className={styles.btnWrapper}>
 				<button title="Rename" className={styles.btnRename} onClick={() => onRenameTodo(bin, id, title)}><HiMiniPencilSquare /></button>
-				<button title='Remove' className={styles.btnRemove} onClick={() => onRemoveTodo(bin, id)}><FaTrash /></button>
+				<button title='Remove' className={styles.btnRemove} onClick={handleRemove}><FaTrash /></button>
 				<button title='Toggle Todo Status' className={styles.btnMark} onClick={() => onMarkTodo(bin, id)}><FaCheck /></button>
 			</div>
-		</div>
+		</li>
 	);
 }
 
