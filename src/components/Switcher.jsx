f7a4356a74d12a57e3e3b6ce7f65b6719d3ff459
Nikolai A. <ilowen@ya.ru>
2024-02-22 20:34:00 +0800

    New: Added the ability to customize the active icon color in the Switcher

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -2,25 +2,47 @@ import styles from '../css/Switcher.module.css';
 
 // components
 import Tooltip from '../components/Tooltip';
+import { useEffect, useRef, useState } from 'react';
 
 function Switcher(props) {
 	const {
 		name,
 
 		iconLeft,
+		activeLeftColor,
 		tooltipLeft,
 		checkedLeft,
 		onChangeLeft,
 
 		iconRight,
+		activeRightColor,
 		tooltipRight,
 		checkedRight,
 		onChangeRight
 	} = props;
 
+	const inputLeftRef = useRef(null);
+	const inputRightRef = useRef(null);
+
+	const [leftColor, setLeftColor] = useState('');
+	const [rightColor, setRightColor] = useState('');
+
+	useEffect(() => {
+		if (inputLeftRef.current) {
+			const color = inputLeftRef.current.checked ? activeLeftColor : '';
+			setLeftColor(color);
+		};
+
+		if (inputRightRef.current) {
+			const color = inputRightRef.current.checked ? activeRightColor : '';
+			setRightColor(color);
+		};
+	}, [inputLeftRef.current?.checked, inputRightRef.current?.checked]);
+
 	return (
 		<div className={styles.switcher}>
 			<input
+				ref={inputLeftRef}
 				className={`${styles.input} ${styles.inputLeft}`}
 				type="radio"
 				name={name}
@@ -30,6 +52,7 @@ function Switcher(props) {
 			/>
 			<Tooltip text={tooltipLeft}>
 				<label
+					style={{ color: leftColor }}
 					className={styles.label}
 					htmlFor={name + 'InputLeft'}
 				>
@@ -38,6 +61,7 @@ function Switcher(props) {
 			</Tooltip>
 
 			<input
+				ref={inputRightRef}
 				className={`${styles.input} ${styles.inputRigth}`}
 				type="radio"
 				name={name}
@@ -47,6 +71,7 @@ function Switcher(props) {
 			/>
 			<Tooltip text={tooltipRight}>
 				<label
+					style={{ color: rightColor }}
 					className={styles.label}
 					htmlFor={name + 'inputRigth'}
 				>
