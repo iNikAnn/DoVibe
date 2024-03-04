e03d06d45b27608630eac79d07a8302e4eb740d7
Nikolai A. <ilowen@ya.ru>
2024-03-04 10:04:13 +0800

    Fix: vertical scroll presence on empty mobile page

--- src/components/FiltersBar.jsx
+++ src/components/FiltersBar.jsx
@@ -15,68 +15,58 @@ import TextBadge from './TextBadge';
 
 function FiltersBar({ initialDate, colorScheme, onChangeScheme, onChangeViewMode, onToggleLeftSideBar, setOnlyUncompleted, leftSideBarIsVisible, isOnlyUncompleted }) {
 	return (
-		<div className={styles.filtersWrapper}>
-			<div className={styles.filtersLeft}>
-				<Filter
-					name="leftSideBar"
-
-					icon={<BsLayoutSidebarInset />}
-					tooltip={leftSideBarIsVisible ? 'Hide sidebar' : 'Show sidebar'}
-					checked={leftSideBarIsVisible}
-					onChange={onToggleLeftSideBar}
-				/>
-
-				<span className={styles.separator}>|</span>
-
-				<TextBadge text={initialDate ? new Date(initialDate).toLocaleDateString() : 'All todos'} />
-			</div>
-
-			<div className={styles.filtersRight}>
-				<Switcher
-					name="colorScheme"
-
-					iconLeft={<FaSun />}
-					activeLeftColor='#ffcc66'
-					tooltipLeft="Light scheme"
-					checkedLeft={colorScheme === 'light'}
-					onChangeLeft={() => onChangeScheme('light')}
-
-					iconRight={<FaMoon />}
-					activeRightColor='#bb88ff'
-					tooltipRight="Dark scheme"
-					checkedRight={colorScheme === 'dark'}
-					onChangeRight={() => onChangeScheme('dark')}
-				/>
-
-				<span className={styles.separator}>|</span>
-
-				<Switcher
-					name="viewMode"
-
-					iconLeft={<FaCalendarAlt />}
-					activeLeftColor='#ff9977'
-					tooltipLeft="Show all todos"
-					checkedLeft={initialDate ? false : true}
-					onChangeLeft={() => onChangeViewMode('')}
-
-					iconRight={<FaCalendarDay />}
-					activeRightColor='#ff9977'
-					tooltipRight="Show todos for the day"
-					checkedRight={initialDate ? true : false}
-					onChangeRight={() => onChangeViewMode(new Date())}
-				/>
-
-				<span className={styles.separator}>|</span>
-
-				<Filter
-					name="onlyUncompleted"
-					icon={<FaListAlt />}
-					tooltip="Show only uncompleted todos"
-					checked={isOnlyUncompleted}
-					onChange={setOnlyUncompleted}
-				/>
-			</div>
-		</div >
+		<div style={{ paddingTop: '1rem' }}>
+			<div className={styles.filtersWrapper}>
+				<div className={styles.filtersLeft}>
+					<Filter
+						name="leftSideBar"
+						icon={<BsLayoutSidebarInset />}
+						tooltip={leftSideBarIsVisible ? 'Hide sidebar' : 'Show sidebar'}
+						checked={leftSideBarIsVisible}
+						onChange={onToggleLeftSideBar}
+					/>
+					<span className={styles.separator}>|</span>
+					<TextBadge text={initialDate ? new Date(initialDate).toLocaleDateString() : 'All todos'} />
+				</div>
+				<div className={styles.filtersRight}>
+					<Switcher
+						name="colorScheme"
+						iconLeft={<FaSun />}
+						activeLeftColor='#ffcc66'
+						tooltipLeft="Light scheme"
+						checkedLeft={colorScheme === 'light'}
+						onChangeLeft={() => onChangeScheme('light')}
+						iconRight={<FaMoon />}
+						activeRightColor='#bb88ff'
+						tooltipRight="Dark scheme"
+						checkedRight={colorScheme === 'dark'}
+						onChangeRight={() => onChangeScheme('dark')}
+					/>
+					<span className={styles.separator}>|</span>
+					<Switcher
+						name="viewMode"
+						iconLeft={<FaCalendarAlt />}
+						activeLeftColor='#ff9977'
+						tooltipLeft="Show all todos"
+						checkedLeft={initialDate ? false : true}
+						onChangeLeft={() => onChangeViewMode('')}
+						iconRight={<FaCalendarDay />}
+						activeRightColor='#ff9977'
+						tooltipRight="Show todos for the day"
+						checkedRight={initialDate ? true : false}
+						onChangeRight={() => onChangeViewMode(new Date())}
+					/>
+					<span className={styles.separator}>|</span>
+					<Filter
+						name="onlyUncompleted"
+						icon={<FaListAlt />}
+						tooltip="Show only uncompleted todos"
+						checked={isOnlyUncompleted}
+						onChange={setOnlyUncompleted}
+					/>
+				</div>
+			</div >
+		</div>
 	);
 }
 
