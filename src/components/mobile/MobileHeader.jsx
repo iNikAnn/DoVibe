45e6aeb9596c006c0687ff788fbe7ebc48d56470
Nikolai A. <ilowen@ya.ru>
2024-03-20 20:43:25 +0800

    Update: Add transition animation for changing viewMode label in mobile version

--- src/components/mobile/MobileHeader.jsx
+++ src/components/mobile/MobileHeader.jsx
@@ -1,7 +1,27 @@
 import styles from '../../css/MobileHeader.module.css';
 
+// framer
+import { AnimatePresence, motion } from 'framer-motion';
+
 function MobileHeader({ date }) {
-	const viewMode = new Date(date).toLocaleDateString();
+	const selectedDate = new Date(date).toLocaleDateString();
+
+	const viewModeVariants = {
+		initial: {
+			opacity: 0,
+			y: '-100%'
+		},
+
+		animate: {
+			opacity: 1,
+			y: 0
+		},
+
+		exit: {
+			opacity: 0,
+			y: '100%'
+		},
+	};
 
 	return (
 		<header className={styles.header}>
@@ -10,7 +30,25 @@ function MobileHeader({ date }) {
 				<h2>DoVibe</h2>
 			</div>
 
-			<span>{date ? viewMode : 'All todos'}</span>
+			<div>
+				<AnimatePresence mode='popLayout'>
+					{date ? (
+						<motion.div
+							key="selectedDate"
+							{...viewModeVariants}
+						>
+							{selectedDate}
+						</motion.div>
+					) : (
+						<motion.div
+							key="allTodos"
+							{...viewModeVariants}
+						>
+							All todos
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</div>
 		</header>
 	);
 }
